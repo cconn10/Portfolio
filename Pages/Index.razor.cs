c14156926ade3993c8addc783ac9b5440e3786f3
shake <shake@DESKTOP-0SO059A>
2022-06-22 18:55:47 -0400

    Add project files.

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using SpotifyAPI.Web;
+
+namespace Portfolio.Pages
+{
+    public partial class Index
+    {
+        SpotifyClient spotify;
+        FullTrack track;
+
+        const string CLIENT_ID = "02b532a8a43748bba871395facd50d11";
+
+        protected override async Task OnInitializedAsync()
+        {
+            var spotify = new SpotifyClient("BQCNzx84qd86DrDksdSdwRNW06gqu621p3l4NDA_rpPJvvxmazqQLn8O1FFyM_pwVesfxyVSTvDAkC1o78UVKqIvQu5LsTzHjUGEvECAbei5p1NLXYU2detYlUbyrQ4oox5gItl");
+
+            var track = await spotify.Tracks.Get("1s6ux0lNiTziSrd7iUAADH");
+            Console.WriteLine(track.Name);
+        }
+    }
+}
